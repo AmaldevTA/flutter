ce62fcbe3567612e7b070998c33062236ee72ea4
Amaldev <amaldev.ta@waypoint-systems.com>
2020-07-01 16:28:33 +0530

    platform channel

---
+++ flutter_platform_channel/android/app/src/main/kotlin/com/wps/flutter_platform_channel/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.wps.flutter_platform_channel
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+const val CHANNEL = "com.wps.flutter_platform_channel/channel_one"
+
+class MainActivity: FlutterActivity(){
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // Note: this method is invoked on the main thread.
+            call, result ->
+            Thread{
+                if (call.method == "getBatteryLevel") {
+                    val batteryLevel = getBatteryLevel()
+
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
+                } else {
+                    result.notImplemented()
+                }
+            }.start()
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        return 95
+    }
+}
---
+++ flutter_platform_channel/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('com.wps.flutter_platform_channel/channel_one');
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          '$_batteryLevel',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getBatteryLevel,
+        tooltip: 'Battery Level',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
